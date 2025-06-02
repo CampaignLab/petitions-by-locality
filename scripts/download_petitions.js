c93694583b73bfe51b854b351aa09219a0810ef1
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2025-06-02 20:07:17 +0100

    update msg

--- scripts/download_petitions.js
+++ scripts/download_petitions.js
@@ -21,7 +21,7 @@ async function fetchAllPetitions(outputPath) {
     const allPetitions = [];
     let count = 0;
 
-    console.log(`Fetching petitions. This may take a while.\nCounter updates every 50.`);
+    console.log(`Fetching petitions. This may take a while.\nCounter updates every 5.`);
 
     while (url) {
         const listPage = await fetchJson(url);
